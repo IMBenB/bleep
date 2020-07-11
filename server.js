7001ebbcacd2474c42ed4c2b61cf930e8c46a9c8
ben <b_brinberg@hotmail.com>
2020-07-11 21:43:50 +0300

    שולח מייל

--- server.js
+++ server.js
@@ -1,8 +1,48 @@
 const express = require('express');
 const app = express();
+const bodyParser = require('body-parser');
+const cors = require("cors");
+const nodemailer = require('nodemailer');
 
+app.use(cors());
 app.use(express.static('public'));
+app.use(bodyParser.urlencoded({ extended: true }));
 
+app.use(bodyParser.json());
+
+app.post('/contact', (req, res) => {
+    let contact = req.body
+    console.log(contact)
+    let smtpTransport = nodemailer.createTransport({
+        service: 'gmail',
+        port:465,
+        auth:{
+            user:'bleeperan@gmail.com',
+            pass:'bleep1234'
+          
+        }
+    });
+    let mailOptions={
+        from:`${contact.email}`,
+        to: `eranpri21@gmail.com`,
+        subject:`Message from ${contact.name}`,
+        html:`
+        <h3>${contact.subject}</h3>
+        <div>${contact.text}</div>
+           
+        `
+    };
+
+    smtpTransport.sendMail(mailOptions, (error,response)=>{
+        if (error){
+            res.send(error)
+        }else{
+            res.send('success')
+        }
+    })
+
+    smtpTransport.close();
+})
 
 let port = process.env.PORT || 4000;
 
